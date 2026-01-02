290f151ff8beced87c94891c75c057a6ae70a109
unknown <anasmejbar275@gmail.com>
2026-01-02 17:19:38 +0100

    Nettoyage et préparation du frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,5 @@
-import { useEffect } from "react";
-
 function App() {
-  useEffect(() => {
-    fetch("http://localhost:5000/api/hello")
-      .then(res => res.json())
-      .then(data => console.log("FROM BACKEND:", data))
-      .catch(err => console.error("ERROR:", err));
-  }, []);
-
-  return <h1>Frontend React</h1>;
+  return <h1>WorkAway</h1>;
 }
 
 export default App;
